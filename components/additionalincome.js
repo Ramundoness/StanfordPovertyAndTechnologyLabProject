5ea4803ecc4f205cbe6464421d4b3f7941b19247
Eric Zhou <ericzhou@stanford.edu>
2019-07-04 10:44:11 +0200

    Minor changes
    Minor changes

--- components/additionalincome.js
+++ components/additionalincome.js
@@ -9,12 +9,23 @@ import {
   Keyboard,
   Picker,
   Button,
-  StatusBar,
+  StatusBar
 } from "react-native";
 
-class AdditionalIncome extends Component {
+import Results from "./results";
+
+import { createSwitchNavigator } from "react-navigation";
+import { createAppContainer } from "react-navigation";
+
+// class Money extends React.Component {
+//   render() {
+//     return <AppContainer />;
+//   }
+// }
+
+export default class AdditionalIncome extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
   render() {
     return (
@@ -22,18 +33,21 @@ class AdditionalIncome extends Component {
         <Text style={styles.title}>
           <Text>Additional Income as a Family Unit</Text>
         </Text>
-        <Text>Include anyone living in your home for whom you are financially responsible
-          (for instance, your children) and your spouse OR anyone in your home who is financially
-          responsible for you (for instance, your parents, step-parents, caretaker relative, or
-          other legal guardian) and their children. You may include family members who are temporarily
-          absent from the home.</Text>
+        <Text>
+          Include anyone living in your home for whom you are financially
+          responsible (for instance, your children) and your spouse OR anyone in
+          your home who is financially responsible for you (for instance, your
+          parents, step-parents, caretaker relative, or other legal guardian)
+          and their children. You may include family members who are temporarily
+          absent from the home.
+        </Text>
         <View style={styles.container}>
           <Text>Please enter any other income:</Text>
           <TextInput
             style={styles.textInput}
             placeholder="Your name"
             maxLength={5}
-            keyboardType='numeric' // change keyboard input type
+            keyboardType="numeric" // change keyboard input type
             onBlur={Keyboard.dismiss}
             onChangeText={this.handleNameChange}
           />
@@ -42,7 +56,7 @@ class AdditionalIncome extends Component {
             style={styles.textInput}
             placeholder="Your name"
             maxLength={5}
-            keyboardType='numeric' // change keyboard input type
+            keyboardType="numeric" // change keyboard input type
             onBlur={Keyboard.dismiss}
             onChangeText={this.handleNameChange}
           />
@@ -51,7 +65,7 @@ class AdditionalIncome extends Component {
             style={styles.textInput}
             placeholder="Your name"
             maxLength={5}
-            keyboardType='numeric' // change keyboard input type
+            keyboardType="numeric" // change keyboard input type
             onBlur={Keyboard.dismiss}
             onChangeText={this.handleNameChange}
           />
@@ -61,45 +75,65 @@ class AdditionalIncome extends Component {
             style={{ height: 50, width: 150 }}
             onValueChange={(itemValue, itemIndex) =>
               this.setState({ language: itemValue })
-            }>
+            }
+          >
             <Picker.Item label="Yes" value="yes" />
             <Picker.Item label="No" value="no" />
           </Picker>
-          <Text>How many students in your family are in college? (Including yourself)</Text>
+          <Text>
+            How many students in your family are in college? (Including
+            yourself)
+          </Text>
           <TextInput
             style={styles.textInput}
             placeholder="Your name"
             maxLength={2}
-            keyboardType='numeric' // change keyboard input type
+            keyboardType="numeric" // change keyboard input type
             onBlur={Keyboard.dismiss}
             onChangeText={this.handleNameChange}
           />
           <Button
-            onPress={null}
+            onPress={() => {
+              this.props.navigation.navigate("Results");
+            }}
             title="Let's Apply!"
             color="#841584"
             accessibilityLabel="Learn more about this purple button"
           />
         </View>
       </View>
-    )
+    );
   }
 }
 
-export default AdditionalIncome;
+// class final_screen extends React.Component {
+//   render() {
+//     return <Results />;
+//   }
+// }
+
+// const AppSwitchNavigator = createSwitchNavigator({
+//   AdditionalIncome: { screen: AdditionalIncome},
+//   Results: { screen: final_screen }
+// });
+
+// const AppContainer = createAppContainer(AppSwitchNavigator);
+
+// export default Money;
+
 const styles = StyleSheet.create({
   container: {
     flex: 5,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    padding: 10,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    padding: 10
     //justifyContent: 'center',
   },
   title: {
     flex: 1,
-    backgroundColor: '#fff',
-    textAlign: 'center',
+    backgroundColor: "#fff",
+    textAlign: "center",
     fontSize: 30,
-    fontWeight: 'bold',
+    fontWeight: "bold"
   }
 });
---
+++ components/results.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import {
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  FlatList,
+  TextInput,
+  Keyboard,
+  Picker,
+  Button,
+  StatusBar
+} from "react-native";
+
+class Results extends Component {
+  state = {};
+  render() {
+    return <View />;
+  }
+}
+
+export default Results;
